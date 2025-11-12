97c3e44f5ec2db8756ed1c463836e1203902d6eb
IBM-Udoc <bineetgdsc@gmail.com>
2025-11-12 20:24:18 +0530

    Add minimal test app

---
+++ test_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="U-DOC Test", layout="wide")
+
+st.title("🏥 U-DOC Test")
+st.write("If you see this, Streamlit Cloud successfully deployed the app!")
+
+st.info("Full U-DOC app loading...")
